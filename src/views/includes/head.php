5e4c4df59a3b13de4fdad3ca52957ffec936a734
vanbasx <ivan.bas.vb@gmail.com>
2024-05-08 21:52:55 +0200

    update

--- src/views/includes/head.php
+++ src/views/includes/head.php
@@ -3,7 +3,7 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Dashboard - <?php echo $page_title ?></title>
+   <title><?php echo $page_title ?> - Dashboard</title>
    <link href="https://fonts.cdnfonts.com/css/inter" rel="stylesheet">
    <script src="https://cdn.tailwindcss.com"></script>
 </head>
--- src/views/login.php
+++ src/views/login.php
@@ -21,7 +21,10 @@
                      <label for="" class="">Password</label>
                      <span class="text-xs text-[#EE0000] hidden">Error</span>
                   </div>
-                  <input type="text" placeholder="Enter your password" class="border-[1px] border-[#CBD5E1] px-3 py-1.5 rounded-md placeholder:text-[#9E9E9E] outline-[#157EFD] shadow-sm">
+                  <div class="flex items-center">
+                     <input type="password" placeholder="Enter your password" class="w-full border-[1px] border-[#CBD5E1] px-3 py-1.5 rounded-md placeholder:text-[#9E9E9E] outline-[#157EFD] shadow-sm">
+                     <button type="button" class="ml-[-27px]"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_24_49)"><mask id="mask0_24_49" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="15" height="15"><path d="M15 0H0V15H15V0Z" fill="white"/></mask><g mask="url(#mask0_24_49)"><path d="M6.70637 3.17074C6.96556 3.14053 7.23031 3.125 7.5 3.125C10.415 3.125 12.7499 4.93915 13.75 7.5C13.5034 8.1315 13.1756 8.71762 12.7779 9.24231M4.07476 4.07465C2.79972 4.85254 1.81319 6.05787 1.25 7.5C2.25009 10.0609 4.58502 11.875 7.5 11.875C8.7735 11.875 9.93625 11.5288 10.9254 10.9253M6.17423 6.17412C5.83489 6.51344 5.625 6.98219 5.625 7.5C5.625 8.53556 6.46444 9.375 7.5 9.375C8.01781 9.375 8.48656 9.16512 8.82587 8.82575" stroke="#9E9E9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M2.5 2.5L12.5 12.5" stroke="#9E9E9E" stroke-width="1.5" stroke-linecap="round"/></g></g><defs><clipPath id="clip0_24_49"><rect width="15" height="15" fill="white"/></clipPath></defs></svg></button>
+                  </div>
                </div>
                <div class="flex justify-between items-center pb-8">
                   <div class="flex items-center gap-2">
@@ -33,7 +36,7 @@
                <button class="bg-black text-white font-bold w-full p-3 rounded-md tracking-wide hover:shadow transition-shadow duration-300">Sign In</button>
             </form>
             <p class="text-center text-[#111827] relative mb-6 before:absolute before:left-0 before:top-1/2 before:h-[1px] before:w-[30%] before:bg-[#CBD5E1] after:absolute after:right-0 after:top-1/2 after:h-[1px] after:w-[30%] after:bg-[#CBD5E1]">Or continue with</p>
-            <a href="" class="text-[#111827] font-medium border-[1px] border-[#CBD5E1] w-full flex p-3 rounded-md hover:shadow transition-shadow duration-300">
+            <a href="" class="text-[#111827] font-medium border-[1px] border-[#CBD5E1] w-full flex p-3 rounded-md hover:shadow-sm transition-shadow duration-300">
                <div class="mx-auto">
                   <div class="flex items-center gap-[10px]">
                      <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_24_68)"><path d="M8.50022 3.36458C9.75397 3.36458 10.8767 3.79668 11.7621 4.63957L14.1881 2.21354C12.7147 0.842917 10.7917 0 8.50022 0C5.17812 0 2.30582 1.90542 0.90686 4.68207L3.73312 6.87436C4.40247 4.85918 6.28312 3.36458 8.50022 3.36458Z" fill="#EA4335"/><path d="M16.6388 8.69495C16.6388 8.13891 16.5856 7.60058 16.5042 7.0835H8.5V10.2781H13.0829C12.8775 11.3264 12.2825 12.2189 11.39 12.821L14.1277 14.946C15.725 13.4656 16.6388 11.2768 16.6388 8.69495Z" fill="#4285F4"/><path d="M3.72936 10.1257C3.55936 9.61216 3.46022 9.06674 3.46022 8.50007C3.46022 7.93341 3.55582 7.38799 3.72936 6.87445L0.903125 4.68213C0.325833 5.82963 0 7.12591 0 8.50007C0 9.87424 0.325833 11.1705 0.906667 12.318L3.72936 10.1257Z" fill="#FBBC05"/><path d="M8.50028 17.0001C10.7953 17.0001 12.7255 16.2456 14.128 14.9423L11.3903 12.8173C10.6288 13.3308 9.64778 13.6319 8.50028 13.6319C6.2832 13.6319 4.40255 12.1373 3.72965 10.1221L0.903397 12.3144C2.3059 15.0946 5.17819 17.0001 8.50028 17.0001Z" fill="#34A853"/></g><defs><clipPath id="clip0_24_68"><rect width="17" height="17" fill="white"/></clipPath></defs></svg>
