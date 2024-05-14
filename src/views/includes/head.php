62ec15fae5642dbb9fe6327a6abd702b4a00a721
vanbasx <ivan.bas.vb@gmail.com>
2024-05-14 22:12:12 +0200

    update

--- src/views/includes/head.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title><?php echo $page_title ?> - Dashboard</title>
-   <link href="https://fonts.cdnfonts.com/css/inter" rel="stylesheet">
-   <script src="https://cdn.tailwindcss.com"></script>
-</head>
