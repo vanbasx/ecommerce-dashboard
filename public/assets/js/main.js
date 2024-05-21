ebb6f6bcfc10ac8e04c6def3170dee8cf7e411d1
vanbasx <ivan.bas.vb@gmail.com>
2024-05-21 21:56:54 +0200

    update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,4 @@
-function menuOpen() {
+function burgerMenu() {
    const btn = document.querySelector('#menu-button');
    const menu = document.querySelector('#menu');
    const body = document.querySelector('body');
@@ -11,10 +11,10 @@ function menuOpen() {
    })
 }
 
-menuOpen();
+burgerMenu();
 
 
-function profileOpen() {
+function profileMenu() {
    const btn = document.querySelector('#profile-btn');
    const menu = document.querySelector('#profile-menu');
 
@@ -36,4 +36,50 @@ function profileOpen() {
    document.addEventListener('click', closeMenuOnClickOutside);
 }
 
-profileOpen();
+profileMenu();
+
+
+
+function dropdownMenu() {
+   const dropdowns = document.querySelectorAll('.dropdown-js');
+
+   function closeAllDropdowns() {
+      dropdowns.forEach(dropdown => {
+         const menu = dropdown.querySelector('.select-menu-js');
+         menu.classList.add('scale-0');
+      });
+   }
+
+   dropdowns.forEach(dropdown => {
+      const select = dropdown.querySelector('.select-js');
+      const menu = dropdown.querySelector('.select-menu-js');
+      const options = dropdown.querySelectorAll('.select-menu-js li');
+      const selected = dropdown.querySelector('.selected-js');
+
+      select.addEventListener('click', () => {
+         closeAllDropdowns();
+         menu.classList.toggle('scale-0');
+      });
+
+      options.forEach(option => {
+         option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            menu.classList.add('scale-0');
+
+            options.forEach(option => {
+               option.classList.remove('bg-[#DFEAF2]');
+            });
+
+            option.classList.add('bg-[#DFEAF2]');
+         });
+      });
+   });
+
+   document.addEventListener('click', (e) => {
+      if (!e.target.closest('.dropdown-js')) {
+         closeAllDropdowns();
+      }
+   });
+}
+
+dropdownMenu();
--- views/pages/admin/products/add.php
+++ views/pages/admin/products/add.php
@@ -20,32 +20,56 @@ include_once ROOT . '/views/includes/titles/title-nobtn.php';
             <label for="price" class="text-[14px] font-medium">Price</label>
             <input type="text" id="price" class="px-4 py-3 border-[1px] border-[#DFEAF2] rounded-[8px] text-[15px] font-medium placeholder:text-[#828282] outline-blue-500" placeholder="Enter product price">
          </div>
+         <!-- DROPDOWN -->
          <div class="flex flex-col max-w-[295px] gap-2 max-[900px]:max-w-full">
             <label for="category" class="text-[14px] font-medium">Category</label>
-            <div class="relative">
-               <button type="button" class="relative w-full h-[48.5px] px-4 py-3 bg-white border-[1px] border-[#DFEAF2] rounded-[8px] text-[15px] text-left font-medium outline-blue-500">
-                  <p></p>
+            <div class="dropdown-js relative">
+               <button type="button" class="select-js relative w-full h-[48.5px] px-4 py-3 bg-white border-[1px] border-[#DFEAF2] rounded-[8px] text-[15px] text-left font-medium outline-blue-500">
+                  <span class="selected-js"></span>
                   <svg class="absolute right-4 top-[21px]" width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L6 6L11 1" stroke="#828282" stroke-width="1.5" stroke-linecap="round"/></svg>
                </button>
-               <div class="absolute bg-white w-full mt-2 shadow-lg py-[10px] px-3 border-[1px] border-[#DFEAF2] rounded-[8px] flex flex-col gap-1">
-                  <button type="button" class="text-left py-[6px] px-2 text-[14px] font-medium cursor-pointer rounded-[4px] hover:bg-[#DFEAF2] transition-colors duration-200">Suits</button>
-                  <button type="button" class="text-left py-[6px] px-2 text-[14px] font-medium cursor-pointer rounded-[4px] hover:bg-[#DFEAF2] transition-colors duration-200">Whatches</button>
-                  <button type="button" class="text-left py-[6px] px-2 text-[14px] font-medium cursor-pointer rounded-[4px] hover:bg-[#DFEAF2] transition-colors duration-200">Glasses</button>
-               </div>
+               <ul class="select-menu-js absolute bg-white w-full mt-2 shadow-lg py-[10px] px-3 border-[1px] border-[#DFEAF2] rounded-[8px] flex flex-col gap-1 scale-0 transition-all duration-200 z-[1]">
+                  <li class="text-left py-[6px] px-2 text-[14px] font-medium cursor-pointer rounded-[4px] hover:bg-[#DFEAF2] transition-colors duration-200">Suits</li>
+                  <li class="text-left py-[6px] px-2 text-[14px] font-medium cursor-pointer rounded-[4px] hover:bg-[#DFEAF2] transition-colors duration-200">Glasses</li>
+                  <li class="text-left py-[6px] px-2 text-[14px] font-medium cursor-pointer rounded-[4px] hover:bg-[#DFEAF2] transition-colors duration-200">Whatches </li>
+               </ul>
             </div>
          </div>
+         <!-- /DROPDOWN -->
+
+         <!-- DROPDOWN -->
          <div class="flex flex-col max-w-[295px] gap-2 max-[900px]:max-w-full">
-            <label for="category" class="text-[14px] font-medium">Size</label>
-            <select name="" id="" class="relative appearance-none px-4 py-3 bg-white border-[1px] border-[#DFEAF2] rounded-[8px] text-[15px] font-medium outline-blue-500">
-               
-            </select>
+            <label for="size" class="text-[14px] font-medium">Size</label>
+            <div class="dropdown-js relative">
+               <button type="button" class="select-js relative w-full h-[48.5px] px-4 py-3 bg-white border-[1px] border-[#DFEAF2] rounded-[8px] text-[15px] text-left font-medium outline-blue-500">
+                  <span class="selected-js"></span>
+                  <svg class="absolute right-4 top-[21px]" width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L6 6L11 1" stroke="#828282" stroke-width="1.5" stroke-linecap="round"/></svg>
+               </button>
+               <ul class="select-menu-js absolute bg-white w-full mt-2 shadow-lg py-[10px] px-3 border-[1px] border-[#DFEAF2] rounded-[8px] flex flex-col gap-1 scale-0 transition-all duration-200 z-[1]">
+                  <li class="text-left py-[6px] px-2 text-[14px] font-medium cursor-pointer rounded-[4px] hover:bg-[#DFEAF2] transition-colors duration-200">Small</li>
+                  <li class="text-left py-[6px] px-2 text-[14px] font-medium cursor-pointer rounded-[4px] hover:bg-[#DFEAF2] transition-colors duration-200">Medium</li>
+                  <li class="text-left py-[6px] px-2 text-[14px] font-medium cursor-pointer rounded-[4px] hover:bg-[#DFEAF2] transition-colors duration-200">Large</li>
+               </ul>
+            </div>
          </div>
+         <!-- /DROPDOWN -->
+
+         <!-- DROPDOWN -->
          <div class="flex flex-col max-w-[295px] gap-2 max-[900px]:max-w-full">
-            <label for="category" class="text-[14px] font-medium">Color</label>
-            <select name="" id="" class="relative appearance-none px-4 py-3 bg-white border-[1px] border-[#DFEAF2] rounded-[8px] text-[15px] font-medium outline-blue-500">
-               
-            </select>
+            <label for="colors" class="text-[14px] font-medium">Colors</label>
+            <div class="dropdown-js relative">
+               <button type="button" class="select-js relative w-full h-[48.5px] px-4 py-3 bg-white border-[1px] border-[#DFEAF2] rounded-[8px] text-[15px] text-left font-medium outline-blue-500">
+                  <span class="selected-js"></span>
+                  <svg class="absolute right-4 top-[21px]" width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L6 6L11 1" stroke="#828282" stroke-width="1.5" stroke-linecap="round"/></svg>
+               </button>
+               <ul class="select-menu-js absolute bg-white w-full mt-2 shadow-lg py-[10px] px-3 border-[1px] border-[#DFEAF2] rounded-[8px] flex flex-col gap-1 scale-0 transition-all duration-200 z-[1]">
+                  <li class="text-left py-[6px] px-2 text-[14px] font-medium cursor-pointer rounded-[4px] hover:bg-[#DFEAF2] transition-colors duration-200">Black</li>
+                  <li class="text-left py-[6px] px-2 text-[14px] font-medium cursor-pointer rounded-[4px] hover:bg-[#DFEAF2] transition-colors duration-200">Blue</li>
+                  <li class="text-left py-[6px] px-2 text-[14px] font-medium cursor-pointer rounded-[4px] hover:bg-[#DFEAF2] transition-colors duration-200">Gray</li>
+               </ul>
+            </div>
          </div>
+         <!-- /DROPDOWN -->
          <div class="p-4 bg-white border-[1px] border-[#DFEAF2] rounded-[8px] max-[900px]:max-w-full">
             <div class="pb-1 flex items-center gap-2">
                <input type="checkbox" name="featured" id="featured" class="w-[15px] h-[15px]">
